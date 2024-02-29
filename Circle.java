0c40565766e9d7e5b4f328bdaa12f8fb39fec5cf
Parth Santosh Tripathi <114149533+parthsantosh2411@users.noreply.github.com>
2024-02-29 09:57:10 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,30 @@
+public class Circle implements Shapes {
+    private double radius;
+    private String color;
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
+
+        Circle circle = new Circle(radius, "red");
+        Rectangle rectangle = new Rectangle(length, width);
+        Triangle triangle = new Triangle(base, height);
+
+        System.out.println("Circle area: " + circle.getArea());
+        System.out.println("Rectangle area: " + rectangle.getArea());
+        System.out.println("Triangle area: " + triangle.getArea());
+
+        scanner.close();
+    }
+}
